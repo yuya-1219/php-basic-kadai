a9ae9725f3694ceaae73fca0e0860bad6d6db7f6
yuya-1219 <o.yuuya1219@gmail.com>
2025-09-24 20:45:54 +0900

    Add files via upload

---
+++ kadai_07/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    $ren = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($ren);
+    ?>
+  </p>
+</body>
+</html>
